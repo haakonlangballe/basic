cbfaf7ab5fb56e073b67758ab24b09328e056082
Haakon Langballe <hlangbal@online.no>
2019-04-15 19:45:00 +0200

    nada

---
+++ Middleware/LoggingMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.Diagnostics;
+
+namespace basic.Middleware
+{
+    public class LoggingMiddleware
+    {
+        public RequestDelegate nextDelegate;
+
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            nextDelegate = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Debug.WriteLine("=== Before ===");
+            await nextDelegate.Invoke(context);
+            Debug.WriteLine("=== After ===");
+        }
+    }
+}
